1b32c369d2f9436d05f08beef0b4f9339237e3ad
kirillEN <81442482+kixxpy@users.noreply.github.com>
2024-07-15 16:30:27 +0300

    Подключик библиотеку axios

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,20 +1,17 @@
+import axios from 'axios';
 import React from 'react';
 import Headling from '../../components/Headling/Headling';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
-import { Product } from '../../components/interfaces/product.interface';
 import { PREFIX } from '../../helpers/API';
+import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
 
 export function Menu() {
 	const [products, setProducts] = React.useState<Product[]>([]);
 	const getMenu = async () => {
 		try {
-			const res = await fetch(`${PREFIX}/products`);
-			if (!res.ok) {
-				return;
-			}
-			const data = (await res.json()) as Product[];
+			const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
 			setProducts(data);
 		} catch (e) {
 			console.error(e);
