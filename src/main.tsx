7a3b30f4067c8c232dd15c333623bb774eaa7e0d
kirillEN <81442482+kixxpy@users.noreply.github.com>
2024-08-26 23:37:36 +0300

    26.08

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, defer, RouterProvider } from 'react-router-dom';
 import { PREFIX } from './helpers/API.ts';
 import './index.css';
+import { AuthLayout } from './layout/AuthLayout/AuthLayout.tsx';
 import { Layout } from './layout/Layout/Layout.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error } from './pages/Error/Error.tsx';
+import { Login } from './pages/Login/Login.tsx';
 import { Products } from './pages/Poduct/Product.tsx';
+import { Register } from './pages/Register/Register.tsx';
 
 // eslint-disable-next-line react-refresh/only-export-components
 const Menu = lazy(() => import('./pages/Menu/Menu.tsx'));
@@ -40,6 +43,20 @@ const router = createBrowserRouter([
 							.then(data => data),
 					});
 				},
+			},
+		],
+	},
+	{
+		path: '/auth',
+		element: <AuthLayout />,
+		children: [
+			{
+				path: 'login',
+				element: <Login />,
+			},
+			{
+				path: 'register',
+				element: <Register />,
 			},
 		],
 	},
