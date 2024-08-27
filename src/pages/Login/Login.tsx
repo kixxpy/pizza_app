c9f19222ab15ebb2787147500131a572d46086a4
kirillEN <81442482+kixxpy@users.noreply.github.com>
2024-08-27 14:21:52 +0300

    Добавил логику получения jwt токена

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,19 +1,51 @@
-import { FormEvent } from 'react';
+import axios, { AxiosError } from 'axios';
+import React, { FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Headling from '../../components/Headling/Headling';
 import Input from '../../components/Input/Input';
+import { PREFIX } from '../../helpers/API';
 import styles from './Login.module.css';
 
-export function Login() {
-	const submit = (e: FormEvent) => {
-		e.preventDefault();
-		console.log(e);
+export type LoginForm = {
+	email: {
+		value: string;
 	};
+	password: {
+		value: string;
+	};
+};
+
+export function Login() {
+	const [error, setError] = React.useState<string | null>();
+
+	const submit = async (e: FormEvent) => {
+		e.preventDefault();
+		setError(null);
+		const target = e.target as typeof e.target & LoginForm;
+		const { email, password } = target;
+		await sentLogin(email.value, password.value);
+	};
+
+	const sentLogin = async (email: string, password: string) => {
+		try {
+			const { data } = await axios.post(`${PREFIX}/auth/login`, {
+				email,
+				password,
+			});
+			console.log(data);
+		} catch (e) {
+			if (e instanceof AxiosError) {
+				setError(e.response?.data.message);
+			}
+		}
+	};
+
 	return (
 		<>
 			<div className={styles['login']}>
 				<Headling>Вход</Headling>
+				{error && <div className={styles['error']}>{error}</div>}
 				<form className={styles['form']} onSubmit={submit}>
 					<div className={styles['field']}>
 						<label htmlFor='email'>Ваш email</label>
@@ -33,7 +65,7 @@ export function Login() {
 					</Button>
 				</form>
 				<div className={styles['links']}>
-					<div>Нет акканута?</div>
+					<div>Нет аккаунта?</div>
 					<Link to='/auth/register'>Зарегистрироваться</Link>
 				</div>
 			</div>
